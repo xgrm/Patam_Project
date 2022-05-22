74beb462b5ff791a26f2e0ac2f299fca052991e2
saarmoseri <xgrm406@gmail.com>
2022-05-22 18:35:34 +0300

    TimeSeries Done

--- Agent/src/TimeSeries/TimeSeries.java
+++ Agent/src/TimeSeries/TimeSeries.java
@@ -1,5 +1,8 @@
 package TimeSeries;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -14,5 +17,35 @@ public class TimeSeries {
         map = new HashMap<>();
         this.props =props;
         Arrays.stream(props).forEach(s->map.put(s,new ArrayList<>()));
+    }
+
+    public void addLine(String line){
+        String[] arrLine = line.split(",");
+        for (int i = 0; i < props.length; i++) {
+            map.get(props[i]).add(Float.parseFloat(arrLine[i]));
+        }
+    }
+    public void exportCSV(String fileName){
+        try {
+            PrintWriter csvFile = new PrintWriter(new File(fileName));
+            for (int i = 0; i < props.length ; i++) {
+                csvFile.print(props[i]);
+                if(i!=(props.length-1))
+                    csvFile.print(",");
+            }
+            csvFile.println("");
+            int rowsNum = map.get(props[0]).size();
+            for (int i = 0; i <rowsNum ; i++) {
+                for (int j = 0; j < props.length ; j++) {
+                    csvFile.print(map.get(props[j]).get(i));
+                    if(j!=(props.length -1))
+                        csvFile.print(",");
+                }
+                csvFile.println("");
+            }
+            csvFile.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
