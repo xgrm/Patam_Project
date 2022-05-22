826036cf07a77c996e671c3a6d5c4018ecf5afcd
saarmoseri <xgrm406@gmail.com>
2022-05-22 19:09:43 +0300

    update the close method name

--- Agent/src/Model/AgentModel.java
+++ Agent/src/Model/AgentModel.java
@@ -114,7 +114,7 @@ public class AgentModel extends Observable implements Model, ClientHandler {
         } catch (IOException | InterruptedException e) {throw new RuntimeException(e);}
     }
 
-    public void closeAgent(){
+    public void closeModel(){
         modelServer.stop();
         this.stop = true;
         outToFG.close();
