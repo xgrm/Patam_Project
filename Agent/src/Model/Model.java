eb7593486544b585a3ce058bbcca2f4a7f81b58c
saarmoseri <xgrm406@gmail.com>
2022-05-22 18:36:40 +0300

    Model Interface

--- Agent/src/Model/Model.java
+++ Agent/src/Model/Model.java
@@ -1,4 +1,11 @@
 package Model;
 
+import TimeSeries.TimeSeries;
+
 public interface Model {
+    public void setAileron(double x);
+    public void setElevator(double x);
+    public void setRudder(double x);
+    public void setThrottle(double x);
+    public TimeSeries getTimeSeries();
 }
