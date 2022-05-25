93881e2736fa355824a1619c4e6ca49e60d7f18a
saarmoseri <xgrm406@gmail.com>
2022-05-25 19:48:26 +0300

    Backend DB

--- BackEnd/src/Main.java
+++ BackEnd/src/Main.java
@@ -1,5 +1,26 @@
+import model.db.DBQueries;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DBQueries db = new DBQueries("dbDetails.txt");
+//        int id =  db.addFlight("TLV-JFK");
+//        try {
+//            Scanner scanner = new Scanner(new File("fly_num.csv"));
+//            scanner.nextLine();
+//            while (scanner.hasNext()){
+//                db.insertRow(id,scanner.nextLine());
+//            }
+//            scanner.close();
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//        db.getFlightById(id);
+//        db.deleteFlightById(1);
+
     }
 }
