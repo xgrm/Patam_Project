418792a81bf582f710cd2c3f4e7171e60ca0959d
noyflaysher <noyflaysher12@gmail.com>
2022-06-14 13:45:42 +0300

    functionality

--- Front/src/main/java/view/TimeCapsuleController.java
+++ Front/src/main/java/view/TimeCapsuleController.java
@@ -1,22 +1,21 @@
 package view;
 
 import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ListView;
+import view.Charts.RegChart;
 
 import java.net.URL;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 import java.util.Set;
 
-public class MonitoringController extends BaseController {
+public class TimeCapsuleController extends BaseController{
+
     @FXML
-    ListView listF;
+    ListView featureListTC;
 
     @FXML
     LineChart changeChart;
@@ -25,25 +24,26 @@ public class MonitoringController extends BaseController {
     LineChart correlationChart;
 
     @FXML
-    ScatterChart scatterc;
+    ScatterChart regChart;
 
-    @Override
-    public void updateUi(Object obj) {}
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        setList("jdfbnhkjd ldjfvdlfjb kdfjlbldkfjbidl maya noy");
-        enterLineChartChange("6 5 8.8 4.5 20 100");
-        enterLineChartCorrelation("5 4 4.4 80 100 775");
-        enterRegChart("8 100 4 55.5 14 50 3 30.5 16 85");
-    }
     public void  setList(String features){
         String[] str=features.split(" ");
         Set<String> s= new HashSet<>();
         for(String string:str){
             s.add(string);
         }
+        featureListTC.getItems().addAll(s);
+    }
 
-        listF.getItems().addAll(s);
+    @Override
+    public void updateUi(Object obj) {}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setList("ggnjn nn bbb cc");
+        enterLineChartChange("6 5 8.8 4.5 20 100");
+        enterLineChartCorrelation("5 4 4.4 80 100 775");
+        enterRegChart("8 100 4 55.5 14 50 3 30.5 16 85");
     }
     public void enterLineChartChange(String data){
         XYChart.Series setLineChart=new XYChart.Series<>();
@@ -68,7 +68,6 @@ public class MonitoringController extends BaseController {
         correlationChart.getData().add(setLineChart);
     }
 
-
     public void enterRegChart(String data) {
         String[] str=data.split(" ");
         System.out.println("enter to reg chart");
@@ -77,7 +76,16 @@ public class MonitoringController extends BaseController {
             series.getData().add(new XYChart.Data(str[i],Double.parseDouble((str[i+1]))));
         }
 
-        scatterc.getData().add(series);
+        regChart.getData().add(series);
+
+        XYChart.Series series2=new XYChart.Series();
+        for(int i=0; i< str.length; i+=2){
+            series2.getData().add(new XYChart.Data(str[i],Double.parseDouble((str[i+1]))+8));
+
+        }
+
+        regChart.getData().add(series2);
     }
+
 
 }
