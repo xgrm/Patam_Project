d183a0c6f6ee16fed3c6174538882851f9b78d30
saarmoseri <xgrm406@gmail.com>
2022-06-14 13:53:50 +0300

    connect to back

--- Front/src/main/java/viewModel/ViewModel.java
+++ Front/src/main/java/viewModel/ViewModel.java
@@ -3,20 +3,50 @@ package viewModel;
 import javafx.beans.property.FloatProperty;
 import model.MainModel;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
 public class ViewModel extends Observable implements Observer {
     MainModel model;
     Socket backend;
+    HashMap<String,String> propMap;
     public FloatProperty aileron, elevator, rudder, throttle;
 
-    public ViewModel(MainModel model) {
+    public ViewModel(MainModel model,String propPath) {
         this.model = model;
+        this.propMap = new HashMap<>();
+        createPropMap(propPath);
+        new Thread(()->{
+            try {
+                backend = new Socket(propMap.get("backEndIP"),Integer.parseInt(propMap.get("backEndPort")));
+                connectToBackend();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
+    private void createPropMap(String propPath){
+        try {
+            Scanner scanner = new Scanner(new File(propPath));
+            String[] tokens;
+            while (scanner.hasNext()){
+                tokens = scanner.nextLine().split(",");
+                this.propMap.put(tokens[0],tokens[1]);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void connectToBackend(){
 
-    private void connectToBackend(){}
+    }
 
     @Override
     public void update(Observable o, Object arg) {
